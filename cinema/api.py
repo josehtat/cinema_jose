1a18b71695ea8dfab7b299356320a5b8b3399de4
josehtat <jhurtadot33@gmail.com>
2025-03-20 19:51:47 +0100

    .

--- cinema/api.py
+++ cinema/api.py
@@ -7,9 +7,7 @@ api = NinjaAPI()
 
 class PeliculaOut(Schema):
     titol : str
-    durada : int
     descripcio : str
-    genere : str
     data_estrena: date 
 
 class PeliculaIn(Schema):
@@ -19,10 +17,27 @@ class PeliculaIn(Schema):
     genere = str
     data_estrena = date
 
+class ButacaOut(Schema):
+    sala : SalaOut
+    fila : int
+    numero : int
+
+class SalaOut(Schema):
+    nom : str
+    capacitat : int
+    butaca : List[ButacaOut]
+
+
+
 @api.get("/pelis", response=List[PeliculaOut])
 def pelicules(request):
     pelicules = Pelicula.objects.all()
     return pelicules
+
+@api.get("/salas", response=List[SalaOut])
+def salas(request):
+    salas = Sala.objects.all()
+    return salas
 
 @api.post("/peli")
 def create_employee(request, payload: PeliculaIn):
